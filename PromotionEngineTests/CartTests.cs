baa5e772002ef68549b84311656dc79226703f86
kalesupriya <supriya.kale@springernature.com>
2021-06-08 17:29:21 +0530

    Added other test cases to cover promotional items & chcek correct cart price calculation

--- PromotionEngineTests/CartTests.cs
+++ PromotionEngineTests/CartTests.cs
@@ -64,6 +64,48 @@ namespace PromotionEngineTests
       //assert
       Assert.AreEqual(result, 21);
     }
-  }
+    [TestMethod("cart with multiple promo items returns promotion price")]
+    public void Should_Return_MultiPromoPriceProducts()
+    {
+      //arrange
+      var promoService = new PromotionService();
+      promoService.Rules = new List<PromotionRule> {
+      new PromotionRule { Products = new List<Product> {new Product("A", 2), new Product("B", 1)}, PromoPrice = 5},
+      new PromotionRule { Products = new List<Product> {new Product("D", 1)}, PromoPrice = 5}
+      };
+      Cart c = new Cart(new List<Product> {
+      new Product("A", 2, 10),
+      new Product("B", 1, 1),
+      new Product("C", 1, 20),
+      new Product("D", 1, 20)
+      }, promoService);
 
+      //act
+      var result = c.GetPrice();
+
+      //assert
+      Assert.AreEqual(result, 30);
+    }
+
+    [TestMethod("cart with no promo items returns actual item prices")]
+    public void Should_Return_ActualProductPrice()
+    {
+      //arrange
+
+      var promoService = new PromotionService();
+      Cart c = new Cart(new List<Product> {
+      new Product("A", 2, 10),
+      new Product("B", 1, 1),
+      new Product("C", 1, 20),
+      new Product("D", 1, 20)
+      }, promoService);
+
+      //act
+      var result = c.GetPrice();
+
+      //assert
+      Assert.AreEqual(result, 61);
+    }
+  }
 }
+
